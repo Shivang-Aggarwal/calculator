60c6951e5d93fa2ab7a09cdf26452d572ca28e79
shivang <shivangaggarwal6208@gmail.com>
2026-02-08 19:30:04 +0530

    Added basic calculation logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,112 @@ import Button from './components/Button'
 
 function App() {
 
-  const [result, setResult] = useState('');
+  const operators: readonly string[] = ['+', '-', 'x', '÷', '%']
+  const [expression, setExpression] = useState<string[]>([])
+
+  function isValidNumber(s: string) {
+    return s.trim() !== "" && !Number.isNaN(Number(s))
+  }
+
+  function containsOperator(items: string[]): boolean {
+    for (const item of items) {
+      if (operators.includes(item)) {
+        return true
+      }
+    }
+
+    return false
+  }
+
+  function evaluate(): string {
+    let x: number | null = null
+    let y: number | null = null
+    let op: string | null = null
+
+    for (const item of expression) {
+      if (operators.includes(item)) {
+        if (op === null) {
+          op = item
+        }
+      } else {
+        if (x === null) {
+          x = Number(item)
+        } else if (y === null) {
+          y = Number(item)
+        }
+      }
+
+      if (x !== null && y !== null && op !== null) {
+        switch (op) {
+          case '+':
+            x = x + y
+            break
+          case '-':
+            x = x - y
+            break
+          case 'x':
+            x = x * y
+            break
+          case '÷':
+            x = x / y
+            break
+          case '%':
+            x = x % y
+            break
+          default:
+            throw new Error("Unable to evaluate expression!")  
+        }
+
+        y = null
+        op = null
+      }
+    }
+
+    if (x !== null) {
+      return x.toString()
+    } else {
+      return Number.MIN_SAFE_INTEGER.toString()
+    }
+  }
 
   const changeResult = (text: string) => {
     if (text === 'AC') {
-      setResult('')
+      setExpression([])
     } else if (text === '⌫') {
-      setResult(result.substring(0, result.length - 1))
+      setExpression(expression.slice(0, expression.length - 1))
+    } else if (text === '=') {
+      let size: number = expression.length
+
+      if (containsOperator(expression) && size % 2 === 1 && size > 1) {
+        setExpression([evaluate()])
+      }
     } else {
-      setResult(result + text)
+      let len: number = expression.length
+
+      if (operators.includes(text)) {
+        if (len > 0 && !operators.includes(expression[len - 1])) {
+          setExpression([...expression, text])
+        }
+      } else if(isValidNumber(text)) {
+        let last: string = expression[len - 1]
+
+        if (len === 0 || operators.includes(last)) {
+          setExpression([...expression, text])
+        } else {
+          setExpression([...expression.slice(0, len - 1), last + text])
+        }
+      }
     }
+
+    // console.log(expression.length)
+    // console.log(expression)
   }
 
   return (
     <div className='flex flex-col justify-center items-center gap-3'>
       <div className='text-xl font-bold'>Calculator</div>
       <div className='w-1/3 grid grid-cols-4 gap-0.5 bg-gray-400'>
-      <input type='text' value={result} disabled={true} className='col-span-full text-right' />
+      <input type='text' value={expression.join("")} disabled={true} className='col-span-full text-right' />
 
       <Button changeResult={changeResult} text='⌫' color='gray' />
       <Button changeResult={changeResult} text='AC' color='gray' />
